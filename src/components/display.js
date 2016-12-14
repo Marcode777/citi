d23c822f34e227a2626267f2b67fdb94dc9af4c8
Marcode777 <marcosebello7@gmail.com>
2016-12-14 11:45:47 -0500

    successfully implemented Display component

---
+++ src/components/display.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Display extends React.Component{
+  render(){
+    return(
+      <div>
+        <h2>INTERFACE</h2>
+      </div>
+      );
+  }
+}
