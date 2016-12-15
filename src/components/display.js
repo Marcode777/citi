503eaf9d9aba55645da575b1e85fe2a90437fb79
Marcode777 <marcosebello7@gmail.com>
2016-12-15 10:25:04 -0500

    played around with some greensock animation

--- src/components/display.js
+++ src/components/display.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import TweenMax from 'gsap';
+
+
 
 export default class Display extends React.Component{
+
   render(){
      const Styled={
         color: 'blue'
       }
     return(
-      <div>
+      <div ref="citi">
         <h2>INTERFACE</h2>
         <h3 style={Styled}>CITI INTERFACE</h3>
+        <input type="text"/>
       </div>
       );
   }
